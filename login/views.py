aa70ded27bceafff99684b0d3ae8a589cb569923
j.thepac <j.thepac@gmail.com>
2021-05-15 06:22:44 -0400

    added user

--- login/views.py
+++ login/views.py
@@ -3,7 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 # from django.http import HttpResponse
 from django.shortcuts import HttpResponse,render
+from django.contrib.auth import authenticate,login,logout
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib import messages
 
 
-def login(request):
+
+
+def loginPage(request):
+    if (request.method=="POST"):
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        user = authenticate(username=username,password=password)
+        if (user is not None):
+            login(request,user)
+            return render(request,"index.html")
+        else:
+            messages.info(request,'Username or Password incorrect')
+
     return render(request,"login.html")
--- register/views.py
+++ register/views.py
@@ -1,29 +1,24 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserFrom
 from django.contrib import messages
-# Create your views here.
+
+
 
 def register(request):
     form = CreateUserFrom()
-    context = {'form': form}
-    if (request.method == "GET"):
-        return render(request,"register.html",context)
-
-
-    elif (request.method=="POST"):
+    if (request.method=="POST"):
         form=UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            # messages.success(request, 'The form is valid.')
-            return render(request, "login.html")
-        else:
-            # print (form.errors)
-            # return render(request, "register.html", {'form': form.errors.values})
-            return HttpResponse(form.errors.values())
-            # return render(request, 'users/contact.html', context={'form': form})
-            # messages.error(request, 'The form is invalid.')
-            # print(form.errors)
-            # return HttpResponse("something went wrong")
+            user= form.cleaned_data.get('username')
+            messages.success(request,"Account was created for "+user)
+            return redirect("loginPage") #     return HttpResponse(form.errors.values())
+
+    context = {'form': form}
+    return render(request,"register.html",context)
+
+
+
 
 
